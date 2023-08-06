24136c6af4f1d1a2911a365df0d25b9d8e7adabc
Gaurav Mishra <79336103+IronCut47@users.noreply.github.com>
2023-08-06 12:02:59 +0530

    Update 7. List comprehension.py

--- Python/7. List comprehension.py
+++ Python/7. List comprehension.py
@@ -6,13 +6,13 @@ if __name__ == '__main__':
     main = []
     
 #Multiple loops solution 
-    # for i in range(0, x+1):
-    #     for j in range(0, y+1):
-    #         for k in range(0, z+1):
-    #             temp = [i, j, k]
-    #             if temp[0] + temp[1] + temp[2] != n:
-    #                 main.append(temp)
-    # print(main)
+    for i in range(0, x+1):
+        for j in range(0, y+1):
+            for k in range(0, z+1):
+                temp = [i, j, k]
+                if temp[0] + temp[1] + temp[2] != n:
+                    main.append(temp)
+    print(main)
 
 #List Comprehension 
     main = [[i, j, k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k != n]

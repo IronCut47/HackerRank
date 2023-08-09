2e997c74f9cb8bf2d8bcac04eabb4db2d2e21d3c
Gaurav Mishra <79336103+IronCut47@users.noreply.github.com>
2023-08-09 19:38:54 +0530

    Update 12. Tuples.py

--- Python/12. Tuples.py
+++ Python/12. Tuples.py
@@ -1,4 +1,4 @@
-# This quesiton is solved in "Pypy3" because for some reason it's not passing the tesh case when done in "Python 3"
+# This quesiton is solved in "Pypy3" because for some reason it's not passing the test case when done in "Python 3"
 if __name__ == '__main__':
     n = int(input())
     integer_list = list(map(int, input().split()))
